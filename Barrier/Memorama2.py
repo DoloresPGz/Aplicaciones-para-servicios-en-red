f3c32f1b07044086e43bfd0de684b1e859ef4c45
DoloresPGz <42900819+DoloresPGz@users.noreply.github.com>
2020-03-05 23:26:46 -0600

    Realiza la espera de la conexión de 2 clentes

---
+++ Barrier/Memorama2.py
@@ -0,0 +1,152 @@
+import random
+import numpy as np
+import time
+
+
+
+class Memorama(object):
+    #Variables globales
+    def __init__(self):
+        self.turno = 0 #0 -> usuario, 1 -> servidor
+        self.size = 0
+        self.tablero = []
+        self.tablero_deamentis = []
+        self.tablero_marca = []
+        self.usuario = 0
+        self.servidor = 0
+        self.bandera1 = 0
+        self.bandera2 = 0
+
+    def leer_palabras(self, nivel):
+        with open("lista_palabras.txt") as file_palabras:
+            palabras = file_palabras.readlines()        
+        #verificamos el nivel del juego para el tamaño del self.tablero
+        if nivel == "principiante":
+            palabras = palabras[:16]
+            self.size = 4
+        if nivel == "avanzado":
+            palabras = palabras[:36]
+            self.size = 6
+
+        random.shuffle(palabras)
+        return palabras
+
+    def compara_tarjetas(self, row1, col1, row2, col2):
+
+
+        if (self.tablero[row1][col1] == self.tablero[row2][col2]):
+            self.tablero_marca[row1][col1] = 1
+            self.tablero_marca[row2][col2] = 1
+            if(self.consultar_turno() == 1):
+                self.servidor = self.servidor + 1
+                self.asignar_turno(1)
+            else:
+                self.usuario = self.usuario + 1
+                self.asignar_turno(0)
+            return True
+        else:
+            if(self.consultar_turno() == 1):
+                self.asignar_turno(0)
+            else:
+                self.asignar_turno(1)
+            if(self.bandera1 == 0):
+                self.tablero_marca[row1][col1] = 0
+            if(self.bandera2 == 0):
+                self.tablero_marca[row2][col2] = 0
+
+            self.bandera1 = 0
+            self.bandera2 = 0
+            return False
+
+    def muestra_tarjetas(self, row1, col1, row2, col2):
+        if(self.tablero_marca[row1][col1] == 1):
+            self.bandera1 = 1
+        if(self.tablero_marca[row2][col2] == 1):
+            self.bandera2 = 1
+        self.tablero_marca[row1][col1] = 1
+        self.tablero_marca[row2][col2] = 1
+
+
+    def asignar_turno(self,tur):
+        self.turno = tur
+
+    def consultar_turno(self):
+        return self.turno
+
+    def juego_terminado(self):
+        res = 0
+        
+        for row in range(0, self.size):
+            for col in range(0, self.size):
+                res = res + int(self.tablero_marca[row][col])
+        
+        return (res == self.size * self.size) 
+            
+    def elige_tarjetas_servidor(self):
+
+        while True:
+            random.seed(time.clock())
+            row1 = random.randint(0, self.size-1)
+            col1 = random.randint(0, self.size-1)
+            row2 = random.randint(0, self.size-1)
+            col2 = random.randint(0, self.size-1)
+        
+            if(self.tablero_marca[row1][col1] == 0 and self.tablero_marca[row2][col2] == 0 and row1 != row2 and col1 != col2):
+                break
+            
+        return [row1, col1, row2, col2]
+        
+
+    def crear_tablero(self, nivel):
+    #obtenemos la lista de palabras para el self.tablero        
+   
+        palabras = self.leer_palabras(nivel)
+
+        for x in range(0, len(palabras)):
+            palabras[x] = ''.join(palabras[x].split())[:]
+
+        #inicializamos self.tablero
+        self.tablero = [ [0 for x in range(0, self.size)] for y in range(0, self.size)]
+        self.tablero_deamentis = [ [0 for x in range(0, self.size)] for y in range(0, self.size)]
+        self.tablero_marca = [ [0 for x in range(0, self.size)] for y in range(0, self.size)]
+
+        for row in range(0, self.size):
+            for col in range(0, self.size):
+                self.tablero[row][col] = palabras[row * self.size + col]
+                self.tablero_deamentis[row][col] = "*****"
+
+
+        
+    def mostrar_tablero(self):
+        tablero_mostrar = [ [0 for x in range(0, self.size + 1)] for y in range(0, self.size + 1)]
+
+        tablero_mostrar[0][0] = "-"
+        for x in range(0, self.size):
+            tablero_mostrar[0][x + 1] = x
+
+        for row in range(0, self.size):
+            tablero_mostrar[row + 1][0] = row
+            for col in range(0, self.size):
+                if(self.tablero_marca[row][col] == 0):
+                    tablero_mostrar[row + 1][col + 1] =  self.tablero_deamentis[row][col]
+                else:
+                    tablero_mostrar[row + 1][col + 1] =  self.tablero[row][col]
+        return tablero_mostrar
+
+    def tablero_string(self, tablero_mostrar):
+        separador = "$$"
+        tablero = []
+        tablero = np.array(tablero_mostrar).ravel()
+        tablero = separador.join(tablero)
+        return tablero
+
+    def marcador(self):
+        res = [str(self.usuario), str(self.servidor)]
+        return res
+
+"""
+memorama = Memorama()
+memorama.crear_tablero("avanzado")
+res = (memorama.tablero_string(memorama.mostrar_tablero()))
+print(res.split("$$"))
+"""
---
+++ Barrier/client2.py
@@ -0,0 +1,139 @@
+import socket
+import time
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65431  # The port used by the server
+buffer_size = 1024
+
+size = 0
+
+def crea_matriz(size, cadena):
+    size = size + 1 
+    tablero = [ [0 for x in range(0, size )] for y in range(0, size )]
+    cadena = cadena.split("$$")
+    for row in range(0, size  ):
+        for col in range(0, size  ):
+            tablero[row][col] = cadena[row * ( size ) + col]
+
+    return tablero
+
+def imprime_matriz(tablero, size):
+    size = size + 1
+    for x in range(0 , size):
+        for y in range(0, size):
+            print(tablero[x][y], end = "\t")
+        print("\n")
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPClientSocket:
+    TCPClientSocket.connect((HOST, PORT))
+    
+    TCPClientSocket.sendall(b"Hello TCP server")
+
+    data = TCPClientSocket.recv(buffer_size)
+    print(data.decode("utf-8")) 
+    TCPClientSocket.sendall(b"Ok")
+    data = TCPClientSocket.recv(buffer_size)
+    print(data.decode("utf-8"))
+    TCPClientSocket.sendall(b"Ok")
+    data = TCPClientSocket.recv(buffer_size)
+    print(data.decode("utf-8"))
+    TCPClientSocket.sendall(b"Ok")
+
+    while (True):
+
+        nivel = int(input("Nivel: "))
+        if(int(nivel) == 1):
+            size = 4
+            break
+        if(int(nivel) == 2):
+            size = 6
+            break
+
+
+    TCPClientSocket.sendall(str(nivel).encode())
+
+    while True:
+        print("tablero actual:")
+        data = TCPClientSocket.recv(buffer_size).decode("utf-8")
+        time.sleep(2)
+        print(len(data))
+        matriz = crea_matriz(size, data)
+        print(imprime_matriz(matriz, size))
+        TCPClientSocket.sendall(b"Ok matriz recibida")
+        data = TCPClientSocket.recv(buffer_size).decode("utf-8")
+        print(data)
+        time.sleep(2)
+        if(data == "TURNO DE USUARIO"):
+            print("USUARIO")
+            data = TCPClientSocket.recv(buffer_size).decode("utf-8")
+            print(data)
+
+            while(True):
+                carta1 = input("")
+                carta1 = carta1.split()
+                if(len(carta1) == 2):
+                    try:
+                        c10 = int(carta1[0])
+                        c11 = int(carta1[1])
+                        if c10 < size and c11 < size:
+                            carta1 = "$$".join(carta1)
+                            TCPClientSocket.sendall(carta1.encode())
+                            break
+                        else:
+                            print("Ingresa una coordenada correcta") 
+                    except Exception as e:
+                        print("Debes ingresar números")
+                else:
+                    print("Ingresa las coordenadas correctas")
+
+            data = TCPClientSocket.recv(buffer_size).decode("utf-8")
+            print(data)
+
+            while(True):
+                carta2 = input("")
+                carta2 = carta2.split()
+                if(len(carta2) == 2):
+                    try:
+                        c20 = int(carta2[0])
+                        c21 = int(carta2[1])
+                        if c20 < size and c21 < size:
+                            carta2 = "$$".join(carta2)
+                            TCPClientSocket.sendall(carta2.encode())
+                            break
+                        else:
+                            print("Ingresa una coordenada correcta") 
+                    except Exception as e:
+                        print("Debes ingresar números")
+                    
+                else:
+                    print("Ingresa las coordenadas correctas")
+
+            
+            data = TCPClientSocket.recv(buffer_size).decode("utf-8")
+            time.sleep(2)
+            matriz = crea_matriz(size, data)
+            print(imprime_matriz(matriz, size))
+
+        if(data == "TURNO DEL SERVIDOR"):
+            print("SERVIDOR")
+            data = TCPClientSocket.recv(buffer_size).decode("utf-8")
+            time.sleep(2)
+            matriz = crea_matriz(size, data)
+            print(imprime_matriz(matriz, size))
+            print("Jugada del servidor")
+            time.sleep(2)
+
+        if(data == "JUEGO TERMINADO"):
+            resultado = TCPClientSocket.recv(buffer_size).decode("utf-8")
+            resultado = resultado.split("$$")
+            print("Usuario:",resultado[0], " | Servidor:", resultado[1])
+            if(int(resultado[0]) > int(resultado[1])):
+                print("Ha ganado el usuario")
+            else:
+                if(int(resultado[0]) == int(resultado[1])):
+                    print("Empate")
+                else:
+                    print("Ha ganado el servidor")
+            TCPClientSocket.close()
+            break
